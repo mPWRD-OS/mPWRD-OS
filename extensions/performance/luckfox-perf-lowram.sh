758d184843c3d39d991fdbc3c70a1711dbc01141
Ruledo <ruledo707@gmail.com>
2026-02-19 18:26:08 -0800

    tuning: drop oom and terminal extensions from import

--- extensions/performance/luckfox-perf-lowram.sh
+++ extensions/performance/luckfox-perf-lowram.sh
@@ -7,14 +7,12 @@
 # - luckfox-perf-apt
 # - luckfox-perf-defaults
 # - luckfox-perf-fstab-noatime
-# - luckfox-perf-terminal
 
 source "$(dirname "${BASH_SOURCE[0]}")/luckfox-perf-journald.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/luckfox-perf-sysctl.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/luckfox-perf-apt.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/luckfox-perf-defaults.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/luckfox-perf-fstab-noatime.sh"
-source "$(dirname "${BASH_SOURCE[0]}")/luckfox-perf-terminal.sh"
 
 function pre_umount_final_image__luckfox_perf_lowram_apply() {
 	pre_umount_final_image__luckfox_perf_journald_apply
@@ -22,7 +20,5 @@ function pre_umount_final_image__luckfox_perf_lowram_apply() {
 	pre_umount_final_image__luckfox_perf_apt_apply
 	pre_umount_final_image__luckfox_perf_defaults_apply
 	pre_umount_final_image__luckfox_perf_fstab_noatime_apply
-	pre_umount_final_image__luckfox_perf_terminal_apply
 	return 0
 }
-
